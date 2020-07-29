663d91a461e9e3eb52d9de5ad61811f9174037c4
Paul Freakn Baker <paul.baker@forcepoint.com>
2020-07-29 12:28:46 -0600

    Better healthcheck and more specificity to the script

--- healthcheck/main.go
+++ healthcheck/main.go
@@ -11,31 +11,29 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"github.com/docker/docker/api/types"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"strings"
+	"syscall"
 )
 import "github.com/docker/docker/client"
 
 func main() {
 	// Note: overriding previous HEALTHCHECK: [CMD-SHELL "$ORACLE_BASE/$CHECK_DB_FILE" >/dev/null || exit 1]
 	// Preserving the original health-check. We want to augment, not overrule
+	nativeHealthCheck()
+	augmentedHealthCheck()
+	log.Println("Oracle is ready")
+}
 
-	// chown the unix socket because we can't configure the
-	// volume to mount with a specific user.
-	// - https://github.com/moby/moby/issues/2259
-	chownCommand := exec.Command("sudo", "chown", "oracle", "/var/run/docker.sock")
-	chownCommand.Stdin = os.Stdin
-	chownCommand.Stdout = os.Stdout
-	chownCommand.Stderr = os.Stderr
-	err := chownCommand.Run()
-	if err != nil {
-		log.Fatalln(err)
-	}
+func augmentedHealthCheck() {
+	fixUnixSocketPermissions()
 	// Create a new docker client and poll the logs. If they
 	// have our magic string, we're in good shape.
 	ctx := context.Background()
@@ -51,16 +49,61 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	logBytes, err := ioutil.ReadAll(logStream)
+	defer logStream.Close()
+	bufferedLogStream := bufio.NewReader(logStream)
+	for {
+		currentLine, err := bufferedLogStream.ReadString('\n')
+		if err != nil {
+			log.Fatalf("oracle is not yet ready to use: %v", err)
+		}
+		if strings.Contains(currentLine, "DATABASE IS READY TO USE!") {
+			return
+		}
+	}
+}
+
+func fixUnixSocketPermissions() {
+	// If we don't own the unix socket as the Oracle user, fix it so we can access it
+	// - https://github.com/moby/moby/issues/2259
+	unixSocketPath := "/var/run/docker.sock"
+	fileInfo, err := os.Stat(unixSocketPath)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	logString := string(logBytes)
-	if !strings.Contains(logString, "DATABASE IS READY TO USE!") {
-		log.Fatalln("oracle is not yet ready to use")
+	if stat, ok := fileInfo.Sys().(*syscall.Stat_t); !ok {
+		log.Fatalf("Could not cast filesystem info to datastructure: %+v", fileInfo.Sys())
+	} else {
+		if unixSocketUID := int(stat.Uid); os.Getuid() != unixSocketUID {
+			chownCommand := exec.Command("sudo", "chown", "oracle", unixSocketPath)
+			chownCommand.Stdin = os.Stdin
+			chownCommand.Stdout = os.Stdout
+			chownCommand.Stderr = os.Stderr
+			err = chownCommand.Run()
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
 	}
-	// I _could_ create a true database connection, but the driver isn't
-	// trivial to implement. I'm going to hedge around it for the time
-	// being by opening a connection via sqlplus instead
-	log.Println("Oracle is ready")
+}
+
+func nativeHealthCheck() {
+	oracleDirectory := mustEnv("ORACLE_BASE")
+	oracleScriptName := mustEnv("CHECK_DB_FILE")
+	fullFilename := path.Join(oracleDirectory, oracleScriptName)
+	originalCommand := exec.Command(fullFilename)
+	originalCommand.Stdin = os.Stdin
+	originalCommand.Stdout = os.Stdout
+	originalCommand.Stderr = os.Stderr
+	if err := originalCommand.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func mustEnv(key string) (value string) {
+	value, isPresent := os.LookupEnv(key)
+	if !isPresent || value == "" {
+		err := fmt.Errorf("there was no value for environment variable: '%s'", key)
+		log.Fatalln(err)
+	}
+	return value
 }
